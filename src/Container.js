2843563e9a26f6367ef8d382968b33f5d415b6e5
Iqbal <iqbalzafar909700@gmail.com>
2021-04-12 17:15:38 +0530

    first commit

---
+++ src/Container.js
@@ -0,0 +1,128 @@
+import React, { Component } from "react";
+import Card from "./Card";
+import "./Container.css";
+
+class Container extends Component {
+  constructor(props) {
+    super(props);
+    const emojis = [
+      "🙉",
+      "🙈",
+      "🙊",
+      "👑",
+      "💎",
+      "⚽",
+      "🙉",
+      "🙈",
+      "🙊",
+      "👑",
+      "💎",
+      "⚽",
+    ];
+    const emojiToShow = emojis
+      .sort(() => Math.random() - 0.5)
+      .map((elem) => {
+        return {
+          content: elem,
+          faceUp: false,
+        };
+      });
+
+    this.state = {
+      emojiToShow: emojiToShow,
+      firstClickId: null,
+      total: emojis.length / 2,
+      score: 1,
+      counter: 0,
+      timer: null,
+      onOff: false,
+    };
+  }
+
+  startCounter() {
+    this.setState({ onOff: !this.state.onOff });
+    this.state.timer = setInterval(() => {
+      this.setState({ counter: this.state.counter + 1 });
+    }, 1000);
+  }
+
+  actualFlipping(idx, faceUp) {
+    this.setState({
+      emojiToShow: this.state.emojiToShow.map((elem, i) => {
+        if (i === idx) {
+          return {
+            content: elem.content,
+            faceUp: !elem.faceUp,
+          };
+        } else {
+          return elem;
+        }
+      }),
+    });
+  }
+
+  flip(idx, faceUp) {
+    if (this.state.onOff === true) {
+      console.log("clicked on", idx);
+      if (this.state.firstClickId === null) {
+        this.setState({ firstClickId: idx });
+      } else {
+        const firstCardData = this.state.emojiToShow[this.state.firstClickId]
+          .content;
+        const secondCardData = this.state.emojiToShow[idx].content;
+        if (firstCardData === secondCardData) {
+          this.setState({ firstClickId: null, score: this.state.score + 1 });
+          if (this.state.total === this.state.score) {
+            clearInterval(this.state.timer);
+            alert(
+              `Congratulations! you have solved this in ${this.state.counter}s`
+            );
+          }
+        } else {
+          setTimeout(() => {
+            this.actualFlipping(this.state.firstClickId);
+            this.actualFlipping(idx);
+            this.setState({ firstClickId: null });
+          }, 2000);
+        }
+      }
+      this.actualFlipping(idx, !this.state.emojiToShow[idx].faceUp);
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Card-Memory-Game</h1>
+        <div className="d-flex justify-content-center mt-5">
+          <div className="card-container  mt-4">
+            {this.state.emojiToShow.map((elem, i) => {
+              return (
+                <Card
+                  key={i}
+                  status={elem.content}
+                  faceUp={elem.faceUp}
+                  flip={() => {
+                    this.flip(i, elem.faceUp);
+                  }}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <h4 className="mt-3">{this.state.counter}s</h4>
+        <button
+          className="btn  mt-2"
+          onClick={() => {
+            this.startCounter();
+          }}
+          disabled={this.state.onOff}
+        >
+          Start Game
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Container;
